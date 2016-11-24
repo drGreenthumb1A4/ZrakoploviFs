138b71fdf56f60935179e6360746dc1eeec41e48
DESKTOP-UNBNLOR\filip <filip.pilot@gmail.com>
2016-11-24 17:36:49 +0100

    Added a lot of stuff

--- Program.fs
+++ Program.fs
@@ -4,9 +4,21 @@ open System
 
 
 type Zrakoplov(reg: string, dest: string, vis: int) =
-    member this.registracija = reg
-    member this.destinacija = dest
-    member this.visina = vis
+    let mutable Reg = reg
+    let mutable Dest = dest
+    let mutable Vis = vis
+
+    member this.registracija 
+      with get () = Reg
+      and set (value) = Reg <- value
+
+    member this.destinacija
+      with get () = Dest
+      and set (value) = Dest <- value
+
+    member this.visina 
+      with get () = Vis
+      and set (value) = Vis <- value
 
 type ZracniKoridor(Ime: string) =
     
@@ -40,9 +52,44 @@ type ZracniKoridor(Ime: string) =
           if zrakoplovi.[i].destinacija = "/" then
              zrakoplovi.[i] <- zrakoplov
              brojZrakoplova <- brojZrakoplova + 1
-
+             i <- 20
+          else i <- i + 1
       ans
   
+    member this.UkloniZrakoplov(reg: string) =
+      let mutable ans = false
+      let mutable i = 0
+      while i<20 do
+          if zrakoplovi.[i].registracija = reg then
+              zrakoplovi.[i].registracija <- "0"
+              zrakoplovi.[i].destinacija <- "/"
+              zrakoplovi.[i].visina <- 0
+              i <- 20
+              ans <- true
+          else i <- i + 1
+      ans
+    
+    member this.IspisiListu =
+      let mutable i = 0 
+      while i < 20 do
+        if not(zrakoplovi.[i].destinacija = "/") then
+          Console.WriteLine("------------------------------------------")
+          Console.WriteLine("Zrakoplov broj " + (i + 1).ToString())
+          Console.WriteLine("Registracija: " + zrakoplovi.[i].registracija)
+          Console.WriteLine("Destinacija: " + zrakoplovi.[i].destinacija)
+          Console.WriteLine("Visina: " + zrakoplovi.[i].visina.ToString())
+          Console.WriteLine("------------------------------------------")
+          i <- i + 1
+    
+    member this.UnesiPodatke = 
+            Console.WriteLine("Unesi registraciju zrakoplova")
+            let reg = Console.ReadLine()
+            Console.WriteLine("Unesi destinaciju zrakoplova")
+            let dest = Console.ReadLine()
+            Console.WriteLine("Unesi visinu zrakoplova")
+            let vis = System.Int32.Parse(Console.ReadLine())
+            let zrakoplov = Zrakoplov(reg, dest, vis)
+            zrakoplov
 
 
 [<EntryPoint>]
